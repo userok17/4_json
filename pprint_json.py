d8af803e3772f36d31f7666ed5a1d0892aa6f458
userok <admin@1rpi.ru>
2017-04-09 02:01:05 +0500

    pprint_json

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,18 @@
 import json
-
+import os
+from pprint import pprint
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, encoding='windows-1251') as data_file:
+        return json.load(data_file)
 
 
 def pretty_print_json(data):
-    pass
+    return pprint(data)
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('data-2897-2016-11-23.json')
+    pretty_print_json(data)
